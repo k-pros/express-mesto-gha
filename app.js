44b951c1116fb2df7c247a5adb3a7251070676d4
Kirill <Kirill.pros@gmail.com>
2023-06-21 23:37:37 +0400

    Добавить логгирование запросов и ошибок

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { login, createUser } = require('./controllers/user');
 const { auth } = require('./middlewares/auth');
 const { validateCreateUser, validateLogin } = require('./middlewares/validation');
 const { handleErrors } = require('./middlewares/handleErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = portNumber } = process.env;
 
@@ -16,11 +17,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 
+app.use(requestLogger); // подключаем логгер запросов
+
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
 
 app.use(auth);
 app.use(router);
+
+app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errors()); // обработчик ошибок celebrate
 app.use(handleErrors); // централизованный обработчик ошибок
